09957a6f568b0ab789b553907eb713b46f885a13
Matheo-bis <matheomarcout@tutamail.com>
2022-12-09 15:37:11 +0100

    Jour 9

--- 9/AoC9.py
+++ 9/AoC9.py
@@ -10,7 +10,6 @@ Covered=[]
 directions={"U":np.array([0,1]),"D":np.array([0,-1]),"L":np.array([-1,0]),"R":np.array([1,0])}
 
 for line in lines:
-    print(line)
     direction, distence = line.split(" ")
     for i in range(int(distence)):
         H+=directions[direction]
@@ -22,7 +21,25 @@ for line in lines:
             elif(T[1]>H[1]): vector+=directions["D"]
             T+=vector
         if T.tolist() not in Covered: Covered.append(T.tolist())
-        print(H)
-        print(T)
-        print()
+print(len(Covered))
+
+Rope=[np.array([0,0]) for i in range(10)]
+Covered=[]
+for line in lines:
+    direction, distence = line.split(" ")
+    for i in range(int(distence)):
+        Rope[0]+=directions[direction]
+        for j in range(1,10):
+            H=Rope[j-1].copy()
+            T=Rope[j].copy()
+            if(np.sqrt(np.sum(np.power(H-T, 2)))>np.sqrt(2)):
+                vector=np.array([0,0])
+                if(T[0]<H[0]): vector+=directions["R"]
+                elif(T[0]>H[0]): vector+=directions["L"]
+                if(T[1]<H[1]): vector+=directions["U"]
+                elif(T[1]>H[1]): vector+=directions["D"]
+                T+=vector
+            Rope[j-1]=H.copy()
+            Rope[j]=T.copy()
+        if Rope[9].tolist() not in Covered: Covered.append(Rope[9].tolist())
 print(len(Covered))
